41ded554b18d15d57757a09adb449da00f39f03d
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-08 12:58:38 +0530

    first-commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 export default function Contact() {
+  const [name,setName]=useState("");
+  const [age,setAge]=useState("");
+  const handleChangeName=(e)=>{
+    // console.log(e.target.value);
+    setName(e.target.value);
+     }
+     const handleChangeAge=(e)=>{
+     setAge(e.target.value);
+     }
+  const handleSubmit=()=>{
+    // console.log("name"); 
+    localStorage.setItem("name", name);
+    localStorage.setItem("age", age);
+  }
+   const handleReset=(e)=>{
+     localStorage.removeItem("name");
+   }
   return (
-    <div>Contact</div>
+    <div><div>Contact</div><br/>
+      <form>
+        <label >Enter the Name:</label>
+        <input type="text" onChange={handleChangeName} required/><br/>
+        <label >Enter the Age:</label>
+        <input type="text" onChange={handleChangeAge} required/><br/>
+        <button type="submit" onClick={handleSubmit}>Submit</button>
+        <button type="reset" onClick={handleReset}>Reset</button>
+        {/* <button type="reset" onClick={handleReset}>Reset</button> */}
+      </form>
+      <p>{localStorage.getItem("name")}</p>
+      <p>{localStorage.getItem("age")}</p>
+    </div> 
   )
 }
